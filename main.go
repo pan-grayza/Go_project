b19069441243dca7b4bb85095cd141abc45c1fc1
Pan-Grayza <zagraystepan@gmail.com>
2023-12-07 09:37:34 +0000

    Another commitment

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Hello world")
+
+	godotenv.Load()
+
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
+		log.Fatal("Port is not found in the environment")
+	}
+
+	fmt.Println("Port: ", portStr)
 }
 
