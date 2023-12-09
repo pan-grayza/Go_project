bb7b5941c6a92a3d38d65d4f9ffa513b29a1a38d
Pan-Grayza <zagraystepan@gmail.com>
2023-12-09 12:53:23 +0000

    Server setup

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/pan-grayza/Go_project
 
 go 1.21.4
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
+
+require github.com/go-chi/cors v1.2.1
--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello world")
-
 	godotenv.Load()
 
 	portStr := os.Getenv("PORT")
@@ -18,6 +18,30 @@ func main() {
 		log.Fatal("Port is not found in the environment")
 	}
 
-	fmt.Println("Port: ", portStr)
-}
+	router := chi.NewRouter()
 
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portStr,
+	}
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
